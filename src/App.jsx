6c756fde6106ddf16883f5f51d6422d125053688
Timur <timur.rakhishov@nu.edu.kz>
2022-12-20 10:52:38 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import SearchBar from './components/SearchBar'
+import './App.css'
+
+function App() {
+
+  return (
+    <SearchBar/>
+  )
+}
+
+export default App
